00012a4ee6eead3109a3a0b227e918e232a3affb
chingun <chingun@umich.edu>
2022-12-18 20:34:22 -0500

    try later

---
+++ trial.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, FlaxCLIPVisionModel
+
+model = FlaxCLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = processor(images=image, return_tensors="np")
+
+outputs = model(**inputs)
+last_hidden_state = outputs.last_hidden_state
+pooler_output = outputs.pooler_output  # pooled CLS states
